eeda6a503700878fe2a66c54ccec97735f4436b4
Steven <stevelaird@live.ca>
2023-04-27 18:33:13 -0500

    Pre-Final Commit

--- Assets/Scripts/Items/MiscClass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewMiscClass", menuName = "Item/Misc")]
-public class MiscClass : ItemClass
-{
-    public override void Use(PlayerController playerCaller) { }
-    public override MiscClass GetMisc() { return this; }
-}
--- Assets/Scripts/Items/RawResourceClass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewRawResourceClass", menuName = "Item/Raw Resource")]
-public class RawResourceClass : ItemClass
-{
-    public override void Use(PlayerController playerCaller) { }
-    public override RawResourceClass GetRawResource() { return this; }
-}
